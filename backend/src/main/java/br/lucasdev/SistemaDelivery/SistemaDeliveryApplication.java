8dc67b9781dea0d6a77f0320cc0be7bd06b40062
Lucas Rodrigues <rs.lucas1@gmail.com>
2021-01-04 18:31:29 -0200

    primeira versao

---
+++ backend/src/main/java/br/lucasdev/SistemaDelivery/SistemaDeliveryApplication.java
@@ -0,0 +1,13 @@
+package br.lucasdev.SistemaDelivery;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SistemaDeliveryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SistemaDeliveryApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/br/lucasdev/SistemaDelivery/SistemaDeliveryApplicationTests.java
@@ -0,0 +1,13 @@
+package br.lucasdev.SistemaDelivery;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SistemaDeliveryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
