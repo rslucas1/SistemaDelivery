ef99e6f246c45ca23abd1e3e2a25a16869411a24
Lucas Rodrigues <rs.lucas1@gmail.com>
2021-01-07 18:56:17 -0200

    Criação do front end

---
+++ front-web/src/App.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import Hello from './Hello'
+
+function App() {
+  return (
+    <div>
+      <h1>Hello World!</h1>
+      <Hello/>
+    </div>
+  );
+}
+
+export default App;
