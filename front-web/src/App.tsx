7c8cb77b17598efae4028aafe085dedbecd0374b
Lucas Rodrigues <rs.lucas1@gmail.com>
2021-01-07 19:51:30 -0200

    Add NavBar

--- front-web/src/App.tsx
+++ front-web/src/App.tsx
@@ -1,14 +1,13 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
-import Hello from './Hello'
+import NavBar from './NavBar'
 
 function App() {
   return (
-    <div>
-      <h1>Hello World!</h1>
-      <Hello/>
-    </div>
+   
+      <NavBar />
+    
   );
 }
 
