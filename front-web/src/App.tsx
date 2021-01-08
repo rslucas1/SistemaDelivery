c4500af5add518cc7f9ba308d3e9df05b05fdae6
Lucas Rodrigues <rs.lucas1@gmail.com>
2021-01-08 11:03:48 -0200

    Add Home

--- front-web/src/App.tsx
+++ front-web/src/App.tsx
@@ -2,12 +2,14 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 import NavBar from './NavBar'
+import Home from './Home';
 
 function App() {
   return (
-   
+    <>
       <NavBar />
-    
+      <Home />
+   </> 
   );
 }
 
